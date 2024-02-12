140f94796e1a27133b7f45aa50feed543af3c913
Shawon301910 <shawonkumarsarkar.bd@gmail.com>
2024-02-12 21:38:57 +0600

    checkNam task completed

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ function calculateMoney(ticketSale) {
     const money = totalTicketPrice - totalCost;
     return money;
 }
+
+
+
+
+function checkName(name) {
+    if (typeof name !== "string") {
+        return "invalid";
+    }
+
+    const changedName = name.toLowerCase();
+    const lastNameIndex = changedName.length - 1;
+
+    if (changedName[lastNameIndex] === "a" || changedName[lastNameIndex] === "y" || changedName[lastNameIndex] === "i" || changedName[lastNameIndex] === "e" || changedName[lastNameIndex] === "o" || changedName[lastNameIndex] === "u" || changedName[lastNameIndex] === "w") {
+        return "Good Name";
+    } else {
+        return "Bad Name";
+    }
+}
